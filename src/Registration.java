87a9e3499c046fd19253315fe6394fbf8fac5297
alere7 <ralex1@xula.edu>
2018-04-25 23:20:47 -0500

    fixing print statements for year and semester counts

--- src/Registration.java
+++ src/Registration.java
@@ -14,13 +14,14 @@ public class Registration {
 
     //todo: (info) select 6 course ==> 1 semester is 6 classes, so intervals of 6 until graduation reqirement is acheieved
 
-    //126 hours to graduate ==> 42 classes
-
     private static ArrayList<String> course;
     private ArrayList<String> coursesTaken;
+    private ArrayList<String> classification;
+    private Random rand = new Random();
     private int loopNum = 3;
     private int numCoursesToGrad = 42;
-    private Random rand = new Random();
+    private int semesterCount = 0;
+    private int classifNum = 0;
 
     public void initalizeCourses() {
         course = new ArrayList<>();
@@ -47,6 +48,13 @@ public class Registration {
             randomCourses(loopNum, "CPSC", course);
             randomCourses(loopNum, "MATH", course);
         }
+
+        classification = new ArrayList<>();
+
+        classification.add("Freshman");
+        classification.add("Sophmore");
+        classification.add("Junior");
+        classification.add("Senior");
     }
 
     public void printCoursesListSize(){
@@ -113,27 +121,44 @@ public class Registration {
         //todo: use a set collection to get rid of duplicates then put into an arraylist (coursesTaken)
         //todo: also make sure CPSC an MATH classes follow the correct order (cant take 1710 and 1720 or 2730 in the same semester)
 
-        //todo: for loop has to be its own function to continue adding classes
-        for (int i = 0; i < numClasses - 1; i++) {
-            coursesTaken.add(c.get(i));
-        }
+        //todo: this needs to be repeated x amount of times
+        addingCourseTaken(numClasses, c);
 
         return coursesTaken;
     }
 
     private void mathCPSCRequiredCourses(ArrayList<String> c, String cName1, String cName2, String cName3){
-        if (!c.contains(cName1))
+        if (!c.contains(cName1)) {
             c.add(cName1);
-        else if (c.contains(cName1) && !c.contains(cName2))
+        }
+        else if (c.contains(cName1) && !c.contains(cName2)) {
             c.add(cName2);
+        }
         else if (c.contains(cName1) && c.contains(cName2)
-                && !c.contains(cName3))
+                && !c.contains(cName3)) {
             c.add(cName3);
+        }
+    }
+
+    private void addingCourseTaken(int numClasses, ArrayList<String> course){
+        for (int i = 0; i < numClasses - 1; i++) {
+            coursesTaken.add(course.get(i));
+        }
     }
 
     //todo: keep track of semester and year count (1st semester freshman year...)
     public void printCoursesTaken(){
-        System.out.println("\nThe list of courses taken are: ");
+        int sc = 1;
+
+        if (semesterCount == 1) {
+            System.out.println(sc + "st Semester " + classification.get(classifNum) + " year");
+            semesterCount++;
+        }
+        else if (semesterCount == 2){
+            System.out.println(sc + "nd Semester " + classification.get(classifNum) + " year");
+            classifNum++;
+        }
+
         for (int i = 0; i < coursesTaken.size(); i++) {
             System.out.println(coursesTaken.get(i));
         }
