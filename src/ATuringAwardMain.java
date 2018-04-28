ede172f9fb4a9e55cc743549d881cdd23838fb44
alere7 <ralex1@xula.edu>
2018-04-27 20:59:49 -0500

    Adding the names to the binary tree

--- src/ATuringAwardMain.java
+++ src/ATuringAwardMain.java
@@ -5,6 +5,22 @@ public class ATuringAwardMain {
     public static void main(String[] args) {
         System.out.println("A Turing Award App");
 
+        String[] names = {"Alan J. Perlis", "Maurice Wilkes", "Richard Hamming", "Marvin Minsky", "James H. Wilkinson",
+                "John McCarthy", "Edsger W. Dijkstra", "Charles W. Bachman", "Donald E. Knuth",
+                "Allen Newell and Herbert A. Simon", "Michael O. Rabin and Dana S. Scott", "John Backus",
+                "Robert W. Floyd", "Kenneth E. Iverson", "Tony Hoare", "Edgar F. Codd", "Stephen A. Cook",
+                "Ken Thompson and Dennis M. Ritchie", "Niklaus Wirth", "Richard M. Karp",
+                "John Hopcroft and Robert Tarjan", "John Cocke", "Ivan Sutherland", "William Kahan",
+                "Fernando J. Corbató", "Robin Milner", "Butler W. Lampson", "Juris Hartmanis and Richard E. Stearns",
+                "Edward Feigenbaum and Raj Reddy", "Manuel Blum", "Amir Pnueli", "Douglas Engelbart", "Jim Gray",
+                "Frederick P. Brooks, Jr.", "Andrew Chi-Chih Yao", "Ole-Johan Dahl and Kristen Nygaard",
+                "Ronald L. Rivest, Adi Shamir and Leonard M. Adleman", "Alan Kay", "Vinton G. Cerf and Robert E. Kahn",
+                "Peter Naur", "Frances E. Allen", "Edmund M. Clarke, E. Allen Emerson and Joseph Sifakis",
+                "Barbara Liskov", "Charles P. Thacker", "Leslie G. Valiant", "Judea Pearl",
+                "Silvio Micali Shafi Goldwasser", "Leslie Lamport", "Michael Stonebraker",
+                "Martin E. Hellman Whitfield Diffie", "Tim Berners-Lee", "John L. Hennessy David A. Patterson"};
+
+        TuringAward tree1 = new TuringAward();
 
     }
 }
---
+++ src/TuringAward.java
@@ -0,0 +1,7 @@
+/**
+ * ralex1 on 4/27/2018.
+ */
+public class TuringAward {
+    private String name;
+    private
+}
