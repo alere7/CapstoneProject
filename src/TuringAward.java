3b79c58de82e768f8c741310ecd2b4a3fc4ebcd8
alere7 <ralex1@xula.edu>
2018-04-29 23:32:05 -0500

    understanding how to apply the bst in this problem

--- src/TuringAward.java
+++ src/TuringAward.java
@@ -7,13 +7,19 @@ public class TuringAward {
     private TuringAward right;
     private int size;
 
+    //todo: add year parameter so you know what year your seaching for
     public TuringAward(String n){
         this.name = n;
         this.left = null;
         this. left = null;
     }
 
-    public void addNode(String i) {
-
+    //todo: how do i know which side of the tree to put the name?
+    //todo: is it by the year they were inducted
+        //todo: ==> the year you searching for also needs to be a peramater when creating the tree
+    public void addNode(String n) {
+//        if (n < this.name){
+//
+//        }
     }
 }
